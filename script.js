adb3c8c1dd725337a7fb253f4a62195a7ece593d
corsatto <101136130+annacorsatto@users.noreply.github.com>
2022-06-27 11:34:49 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-setTimeout(() => {
-  toast.innerHTML = "Calculator";
-}, 1500);
-if (theme.getAttribute("href") === lightTheme) {
-  themeIcon.setAttribute("src", sunIcon);
-  toast.innerHTML = "Dark Mode 🌙";
-} else {
-  theme.setAttribute("href", lightTheme);
-  themeIcon.setAttribute("src", moonIcon);
